5c8dc6a1c5c79f7f3e547a6eb641849332226d9d
mmalloy <mmalloy@zynga.com>
2011-07-22 16:40:13 -0700

    Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,8 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       @title = "Sign Up"
+      @user.password = ""
+      @user.password_confirmation = ""
       render 'new'
     end
   end
