d68e6625a1b31eb79afe717fce350b0f16903629
mikemalloy <mmalloy@zynga.com>
2011-10-08 16:30:07 -0700

    Beginning to change perf test show view

--- app/helpers/performance_tests_helper.rb
+++ app/helpers/performance_tests_helper.rb
@@ -7,14 +7,19 @@ module PerformanceTestsHelper
         output += ","
       end
       if measure == 1 
+        output += "," unless i == 0 or ptr.measure1avg == nil
         output += "[#{i},#{ptr.measure1avg}]" if ptr.measure1avg != nil
       elsif measure == 2
+        output += "," unless i == 0 or ptr.measure2avg == nil
         output += "[#{i},#{ptr.measure2avg}]" if ptr.measure2avg != nil
       elsif measure == 3
+        output += "," unless i == 0 or ptr.measure3avg == nil
         output += "[#{i},#{ptr.measure3avg}]" if ptr.measure3avg != nil
       elsif measure == 4
+        output += "," unless i == 0 or ptr.measure4avg == nil
         output += "[#{i},#{ptr.measure4avg}]" if ptr.measure4avg != nil
       elsif measure == 5
+        output += "," unless i == 0 or ptr.measure5avg == nil
         output += "[#{i},#{ptr.measure5avg}]" if ptr.measure5avg != nil
       end
       i += 1
@@ -27,18 +32,20 @@ module PerformanceTestsHelper
     output = "["
     i = 0
     performance_test_results.each do |ptr|
-      unless i == 0
-        output += ","
-      end
       if measure == 1
+        output += "," unless i == 0 or ptr.measure1max == nil
         output += "[#{i},#{ptr.measure1max}]" if ptr.measure1max != nil
       elsif measure == 2
+        output += "," unless i == 0 or ptr.measure2max == nil
         output += "[#{i},#{ptr.measure2max}]" if ptr.measure2max != nil
       elsif measure == 3
+        output += "," unless i == 0 or ptr.measure3max == nil
         output += "[#{i},#{ptr.measure3max}]" if ptr.measure3max != nil
       elsif measure == 4
+        output += "," unless i == 0 or ptr.measure4max == nil
         output += "[#{i},#{ptr.measure4max}]" if ptr.measure4max != nil
       elsif measure == 5
+        output += "," unless i == 0 or ptr.measure5max == nil
         output += "[#{i},#{ptr.measure5max}]" if ptr.measure5max != nil
       end
       i += 1
@@ -51,18 +58,20 @@ module PerformanceTestsHelper
     output = "["
     i = 0
     performance_test_results.each do |ptr|
-      unless i == 0
-        output += ","
-      end
       if measure == 1
+        output += "," unless i == 0 or ptr.measure1min == nil
         output += "[#{i},#{ptr.measure1min}]" if ptr.measure1min != nil
       elsif measure == 2
+        output += "," unless i == 0 or ptr.measure2min == nil
         output += "[#{i},#{ptr.measure2min}]" if ptr.measure2min != nil
       elsif measure == 3
+        output += "," unless i == 0 or ptr.measure3min == nil
         output += "[#{i},#{ptr.measure3min}]" if ptr.measure3min != nil
       elsif measure == 4
+        output += "," unless i == 0 or ptr.measure4min == nil
         output += "[#{i},#{ptr.measure4min}]" if ptr.measure4min != nil
       elsif measure == 5
+        output += "," unless i == 0 or ptr.measure5min == nil
         output += "[#{i},#{ptr.measure5min}]" if ptr.measure5min != nil
       end
       i += 1
@@ -84,34 +93,6 @@ module PerformanceTestsHelper
     output += "]"
   end 
   
-  def create_chart_string( performance_test_results, index )
-    output = ""
-    output += "var d1 = " + max_values(performance_test_results, index) + ";"
-    output += "var d2 = " + avg_values(performance_test_results, index) + ";"
-    output += "var d3 = " + min_values(performance_test_results, index) + ";"
-    graph_name = '\"measure' + index.to_s + 'graph\"'
-    output += "$.plot($(" + graph_name + '), [ 
-  		{ label: \"Max\", data: d1},
-  		{ label: \"Avg\", data:d2},
-  		{ label: \"Min\", data:d3} 
-  		], {
-  		  series: {
-  			lines: { show: true },
-  			points: { show: true }
-  		},
-  		  xaxis: {
-  			ticks: ' + tick_values(performance_test_results) + '
-  	      },
-  	      legend: {
-  		    position: \"nw\",
-  		    margin: 0,
-  		    noColumns: 6
-  	      }	
-  		}
-  	);'
-    return output
-  end
- 
- 
+  
    
 end
